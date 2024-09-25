52977ceea1fecbc3b9747d40d7a267d315b3966d
Your Name <ansokwin@gmail.com>
2024-09-26 00:24:50 +0700

    1

--- resources/views/backend/invoice/index.blade.php
+++ resources/views/backend/invoice/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoice</title>
+    <title>Invoice | Laor-Prornit-Clinic-Dental</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href=" {{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href=" {{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
